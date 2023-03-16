d26696e7e44bd52fc2c3c0f76c94b4cf3f6f1dba
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2023-03-16 11:28:46 +0100

    Test Maven build

--- Belote2023/src/main/java/com/github/itellijlover/Fenetre.java
+++ Belote2023/src/main/java/com/github/itellijlover/Fenetre.java
@@ -35,7 +35,7 @@ public class Fenetre extends JFrame {
 	public JPanel c;
 	Statement s;
 
-	private JTextArea gt;
+	private JTextArea gtt;
 	private JPanel ListeTournois;
 	private Vector<String> noms_tournois;
 	private JList<String> list;
