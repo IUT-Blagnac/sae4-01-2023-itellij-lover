34e8fed670b8a8886bc0e57192211a06f727080c
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2023-04-08 15:48:50 +0200

    public is redundant for interfaces

--- Belote2023/src/main/java/com/github/itellijlover/dao/DAO.java
+++ Belote2023/src/main/java/com/github/itellijlover/dao/DAO.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public interface DAO<T> {
 
-    public void add(T obj);
+    void add(T obj);
 
-    public void update(T obj);
+    void update(T obj);
 
-    public void delete(int id);
+    void delete(int id);
 
-    public List<T> getAllFromTournoi(int id_tournoi);
+    List<T> getAllFromTournoi(int id_tournoi);
 
 }
